d5348289ca0b8e718d2b5ca491064b22163cde86
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2022-09-03 13:39:17 -0300

    Update SQL 1 #08.sql

--- Curso Banco de Dados/SQL 1 #08.sql
+++ Curso Banco de Dados/SQL 1 #08.sql
@@ -1,4 +1,59 @@
 /**
+Os exercícios ficam ao final da tabela
+*/
+
+DROP DATABASE IF EXISTS aula_banco; -- se existir elimine aula_banco
+CREATE DATABASE aula_banco; 		-- criar aula_banco
+USE aula_banco;						-- selecionar aula_banco
+
+CREATE TABLE estado( 				-- criando a tabela estado
+id INT NOT NULL AUTO_INCREMENT
+,nome VARCHAR(200) NOT NULL UNIQUE  
+,sigla CHAR(2) NOT NULL UNIQUE
+,ativo ENUM('S','N') NOT NULL DEFAULT 'S'
+,data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+,CONSTRAINT pk_estado PRIMARY KEY (id)  
+);
+
+CREATE TABLE cidade (				-- criando a tabela cidade
+id INT NOT NULL AUTO_INCREMENT
+,nome VARCHAR(200)  NOT NULL
+,ativo ENUM('S','N') NOT NULL DEFAULT 'S'
+,data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+,estado_id INT NOT NULL 
+,CONSTRAINT pk_cidade PRIMARY KEY (id)
+,CONSTRAINT fk_cidade_estado FOREIGN KEY (estado_id) REFERENCES estado (id)
+,CONSTRAINT cidade_unica UNIQUE(nome, estado_id)
+);
+
+-- inserindo estados
+INSERT INTO estado (nome,sigla) VALUES ('PARANÁ','PR');
+INSERT INTO estado (nome,sigla) VALUES ('SÃO PAULO','SP');
+INSERT INTO estado (nome,sigla) VALUES ('MATO GROSSO','MT');
+INSERT INTO estado (nome,sigla) VALUES ('SANTA CATARINA','SC');
+INSERT INTO estado (nome,sigla) VALUES ('RIO GRANDE DO SUL','RS');
+
+-- inserindo cidades
+INSERT INTO cidade (nome, estado_id) VALUES ('BAURU',2);
+INSERT INTO cidade (nome, estado_id) VALUES ('MARINGÁ',1);
+INSERT INTO cidade (nome, estado_id) VALUES ('GUARULHOS',2);
+INSERT INTO cidade (nome, estado_id) VALUES ('CAMPINAS',2);
+INSERT INTO cidade (nome, estado_id) VALUES ('FLORIANÓPOLIS',4);
+INSERT INTO cidade (nome, estado_id) VALUES ('PARANAVAÍ',1);
+INSERT INTO cidade (nome, estado_id) VALUES ('CUIABA',3);
+INSERT INTO cidade (nome, estado_id) VALUES ('BALNEÁRIO CAMBORIÚ',4);
+INSERT INTO cidade (nome, estado_id) VALUES ('LONDRINA',1);
+INSERT INTO cidade (nome, estado_id) VALUES ('CASCAVEL',1);
+INSERT INTO cidade (nome, estado_id) VALUES ('JOINVILLE',4);
+INSERT INTO cidade (nome, estado_id) VALUES ('PORTO ALEGRE',5);
+INSERT INTO cidade (nome, estado_id) VALUES ('BLUMENAL',4);
+INSERT INTO cidade (nome, estado_id) VALUES ('BARRA DOS GARÇAS',3);
+INSERT INTO cidade (nome, estado_id) VALUES ('CHAPECÓ',4);
+INSERT INTO cidade (nome, estado_id) VALUES ('ITAJAÍ',4);
+
+
+
+/**
 EXERCÍCIOS DE INSERT
 */
 -- 6.	Escreva o comando para seleccionar todos os registros da tabela cidade com todas as colunas.
@@ -8,5 +63,23 @@ SELECT * FROM cidade;
 SELECT nome FROM cidade;
 
 /**
-
+EXERCÍCIOS DELETE E UPDATE
 */
+-- 9.	Escreva o comando para alterar o valor de uma coluna de um único registro de uma tabela utilizando como filtro, o Primary Key
+UPDATE estado SET ativo = 'N' WHERE id = 1;
+
+-- 10.	Refaça o exercício anterior alterando os dados de mais que uma coluna de um único registro sem utilizar como filtro a chave primária. A escolha da coluna do filtro é muito importante – TOME CUIDADO.
+UPDATE estado SET ativo = 'S' WHERE estado.sigla = 'PR';    
+SELECT * FROM estado;
+
+/**
+EXERCÍCIOS SELECT
+*/
+-- 6.	Escreva o comando para seleccionar todos os registros da tabela cidade com todas as colunas.
+SELECT * FROM cidade; -- se eu não souber quais são todas as colunas.
+-- OU eu posso escrever assim
+SELECT id,nome,ativo,data_cadastro,estado_id FROM cidade;
+
+-- 7.	Escreva o comando para seleccionar o nome de todos os registros da tabela cidade.
+SELECT nome FROM cidade;
+	
