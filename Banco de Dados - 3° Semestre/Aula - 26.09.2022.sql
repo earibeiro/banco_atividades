47da6a7da79dd9f4e4e4848ad292d5e43c46262a
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2022-09-30 23:17:36 -0300

    Update Aula - 26.09.2022.sql

--- Banco de Dados - 3° Semestre/Aula - 26.09.2022.sql
+++ Banco de Dados - 3° Semestre/Aula - 26.09.2022.sql
@@ -74,17 +74,31 @@ WHERE produto.marca_id = marca.id
 AND produto.marca_id = '1';
 
 13. DESAFIO!!! Liste os nomes dos clientes do estado de São Paulo que já compraram o produto 'REFRIGERANTE COCA-COLA GARRAFA PET 3 L'.
-
+SELECT cliente.nome nome_cliente, estado.nome nome_estado, produto.nome nome_produto
+FROM cliente, estado, produto
+WHERE produto.id = 10 AND estado.id = 2;
 
 
 Semântica
 1.	Em que caso devemos utilizar o JOIN? Quais tabelas podem ser utilizando em um comando JOIN?
+O JOIN deve ser utilizado sempre que houver interesse em realizar buscas em uma ou mais tabelas de uma única vez. As tabelas poder receber o JOIN quando existir
+relacionamento entre elas, ou seja, uma deve possuir dependência(FOREIGN KEY) da outra.
 
-2.	No caso do JOIN, o que é condição de junção. Qual cuidado devemos ter. 
 
-3.	Em relação a sintaxe, em um JOIN não é necessário utilizarmos condições (WHERE) - o comando irá executar normalmente. Porém, em questão de semântica, a cada junção é necessário ter ao menos 1 condição. Explique.
+2.	No caso do JOIN, o que é condição de junção. Qual cuidado devemos ter.
+Uma condição de junção define o modo como duas tabelas são relacionadas em uma consulta por: Especificando a coluna de cada tabela a ser usada para a junção. 
+Uma condição de junção típica especifica uma chave estrangeira de uma tabela e sua chave associada na outra tabela.
+
+
+3.	Em relação a sintaxe, em um JOIN não é necessário utilizarmos condições (WHERE) - o comando irá executar normalmente. 
+Porém, em questão de semântica, a cada junção é necessário ter ao menos 1 condição. Explique.
+
+O WHERE serve para filtrar dados em diferentes tabelas do banco de dados, enquanto o JOIN busca tão somente unir as colunas.
 
 4.	O que é produto cartesiano? Como funciona? Qual a relação com o JOIN.
+Resulta em uma combinação de todas as duplas entre as duas relações de entrada, e é utilizado quando se necessita obter dados presentes em duas ou mais relações.
+O produto cartesiano é conhecido como conexão cruzada e resulta no cruzamento entre uma linha de uma tabela com todas as outras de outra tabela.
+O JOIN realiza tal função quando utilizamos os comandos CROSS JOIN ou INNER JOIN.
 
 5.	Na elaboração de um consulta que envolve 1587 tabelas, serão necessários, quantas condições de junção?
 Serão necessárias no mínimo 1586, vez que o valor para condições de junção é igual ao valor de (tabelas - 1).
